cdcbd8f3b99b7cb4d152444513a2c265a5115c07
anand <anandvaibhaw000@gmail.com>
2025-02-19 08:52:51 +0530

    fixed className

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -570,10 +570,10 @@ function Form() {
                 />
               </label> */}
 
-              <label class="flex justify-between cursor-pointer">
-                <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Set event to private</span>
-                <input type="checkbox" value="" class="sr-only peer" />
-                <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"></div>
+              <label className="flex justify-between cursor-pointer">
+                <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Set event to private</span>
+                <input type="checkbox" value="" className="sr-only peer" />
+                <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"></div>
               </label>
 
             </div>
